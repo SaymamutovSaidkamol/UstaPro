03472d31a371846dd228930dff148430574a2966
saymamutovsaidakmol@gmail.com <116891087+saidkamol2006@users.noreply.github.com>
2025-04-14 01:36:10 +0500

    prisma schema changet

--- src/regions/regions.controller.ts
+++ src/regions/regions.controller.ts
@@ -3,12 +3,16 @@ import { RegionsService } from './regions.service';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/decorators/role.decorator';
+import { Role } from 'src/Enums/role.enum';
+import { RoleGuard } from 'src/auth/role.guard';
 
 @Controller('regions')
 export class RegionsController {
   constructor(private readonly regionsService: RegionsService) {}
 
-  @UseGuards(AuthGuard)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Post()
   create(@Body() createRegionDto: CreateRegionDto) {
     return this.regionsService.create(createRegionDto);
@@ -24,11 +28,17 @@ export class RegionsController {
     return this.regionsService.findOne(id);
   }
 
+  
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateRegionDto: UpdateRegionDto) {
     return this.regionsService.update(id, updateRegionDto);
   }
 
+  
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RoleGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.regionsService.remove(id);
